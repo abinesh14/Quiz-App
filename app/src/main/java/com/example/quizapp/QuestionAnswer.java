d8d3c3c655072d1ccb96e2a8a753afd5c6071d9a
abinesh14 <abineshodc@gmail.com>
2024-10-24 17:20:13 +0530

    first commit

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,44 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question [] = {
+            "Who is the founder of NVIDIA?",
+            "In which year was ChatGPT launched?",
+            "Who are the founders of Google?",
+            "Who is the creator of the Linux operating system?",
+            "Which company developed the Java programming language?",
+            "Which city is known as the Silicon Valley of India?",
+            "Who is the founder of Zoho Corporation?",
+            "Who is the CEO of OpenAI?",
+            "Which is considered one of the best startup companies in Bangalore?",
+            "What does LLM stand for in AI?"
+    };
+
+    public static String choices [][] = {
+            {"Jeff Bezos", "Sundar Pichai", "Elon Musk", "Jensen Huang"},
+            {"2020", "2021", "2022", "2023"},
+            {"Mark Zuckerberg & Eduardo Saverin", "Steve Jobs & Steve Wozniak", "Bill Gates & Paul Allen", "Larry Page & Sergey Brin"},
+            {"Linus Torvalds","Ken Thompson", "Dennis Ritchie", "Richard Stallman"},
+            {"Oracle", "Microsoft", "Google", "Sun Microsystems"},
+            {"Hyderabad", "Bangalore", "Pune", "Chennai"},
+            {"Ratan Tata", "Sridhar Vembu", "Mukesh Ambani", "Narayana Murthy"},
+            {"Satya Nadella", "Elon Musk","Sam Altman", "Jeff Bezos"},
+            {"Flipkart", "Byju's", "All the Above", "Zomato"},
+            {"Logical Learning Machine", "Large Language Model", "Language Learning Mechanism", "Long Linear Model"}
+    };
+
+
+    public static String correctAnswers [] = {
+            "Jensen Huang",
+            "2022",
+            "Larry Page & Sergey Brin",
+            "Linus Torvalds",
+            "Sun Microsystems",
+            "Bangalore",
+            "Sridhar Vembu",
+            "Sam Altman",
+            "All the Above",
+            "Large Language Model"
+    };
+}
